a353b554dcb6264fcb9da422d9eba2e7d56cf5fe
Vadim <vnamnyak@gmail.com>
2015-09-28 15:11:03 -0400

    added Firefox support

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ var signals = require('signals');
 var MouseWheel = function(targetElement) {
 	var onWheelSignal = new signals.Signal();
 	EventUtil.addEvent(targetElement, 'mousewheel', onElementMouseWheel );
+	EventUtil.addEvent(targetElement, 'DOMMouseScroll', onElementMouseWheel ); // Firefox
 
 	function onElementMouseWheel( event ) {
-		onWheelSignal.dispatch(event.wheelDelta, event);
+		var delta;
+
+		if (event.wheelDelta !== undefined ) {
+			delta = event.wheelDelta;
+		} else if ( event.detail !== undefined ) { // Firefox
+			delta = -30 * event.detail;
+		}
+
+		onWheelSignal.dispatch(delta, event);
 	};
 	this.onWheelSignal = onWheelSignal;
 };
